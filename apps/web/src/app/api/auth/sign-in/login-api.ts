c672bd9f2542953fea0fed5cf56bab1528d08dac
SeemaPatel-iViewLabs <seema.patel@iviewlabs.com>
2024-11-05 16:26:21 +0530

    resolve cookie set error

--- apps/web/src/app/api/auth/sign-in/login-api.ts
+++ apps/web/src/app/api/auth/sign-in/login-api.ts
@@ -43,9 +43,9 @@ export async function getUserByEmail(data: { email: string; password: string }):
     const refreshToken = generateRefreshToken({ id: user.contactEmail ? user.contactEmail : '' });
 
     const cookieStore = cookies();
-    cookieStore.set(TOKEN_KEY, accessToken);
-    cookieStore.set(REFRESH_TOKEN_KEY, refreshToken);
-    cookieStore.set(USER_ID, sign({ id: user.id }, env.ACCESS_TOKEN_SECRET));
+    cookieStore.set(TOKEN_KEY, accessToken, { httpOnly: true, secure: true });
+    cookieStore.set(REFRESH_TOKEN_KEY, refreshToken, { httpOnly: true, secure: true });
+    cookieStore.set(USER_ID, sign({ id: user.id }, env.ACCESS_TOKEN_SECRET), { httpOnly: true, secure: true });
 
     return {
       success: true,
