6accc36d6f8ec923542d2f56a065d86f6c9a013c
SeemaPatel-iViewLabs <seema.patel@iviewlabs.com>
2024-11-05 17:38:30 +0530

    changes in set cookies

--- apps/web/src/app/api/auth/sign-in/login-api.ts
+++ apps/web/src/app/api/auth/sign-in/login-api.ts
@@ -3,7 +3,7 @@ import { prisma } from '@repo/database';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { logger } from '@repo/logger';
-import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 import { TOKEN_KEY, REFRESH_TOKEN_KEY, USER_ID } from '@/app/modules/constant';
 import { env } from '@/env.mjs';
 import { generateAccessToken, generateRefreshToken } from './login-helper';
@@ -42,18 +42,15 @@ export async function getUserByEmail(data: { email: string; password: string }):
     const accessToken = generateAccessToken({ id: user.contactEmail ?? '' });
     const refreshToken = generateRefreshToken({ id: user.contactEmail ?? '' });
 
-    // Create a response to set cookies
-    const response = NextResponse.json({
-      success: true,
-      token: accessToken,
-      refreshToken,
-      id: user.id,
-    });
+    const cookieStore = await cookies();
 
-    // Set tokens in cookies
-    response.cookies.set(TOKEN_KEY, accessToken, { httpOnly: true, secure: true });
-    response.cookies.set(REFRESH_TOKEN_KEY, refreshToken, { httpOnly: true, secure: true });
-    response.cookies.set(USER_ID, sign({ id: user.id }, env.ACCESS_TOKEN_SECRET), { httpOnly: true, secure: true });
+    cookieStore.set(TOKEN_KEY, accessToken);
+    cookieStore.set(REFRESH_TOKEN_KEY, refreshToken);
+    cookieStore.set(USER_ID, sign({ id: user.id }, env.ACCESS_TOKEN_SECRET), {
+      httpOnly: true,
+      secure: false,
+      path: '/sign-in',
+    });
 
     return {
       success: true,
