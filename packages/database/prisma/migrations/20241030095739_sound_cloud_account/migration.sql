e8524497ef22616b167297327086aaee70186df5
SeemaPatel-iViewLabs <seema.patel@iviewlabs.com>
2024-11-05 11:42:40 +0530

    added new table for sound cloud token

---
+++ packages/database/prisma/migrations/20241030095739_sound_cloud_account/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `sound_cloud_account` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `access_token` VARCHAR(191) NOT NULL,
+    `refresh_token` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `token_created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `token_updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
