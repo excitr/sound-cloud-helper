7e2b26d971de0ae3f1037430ff7a3b6791bdad22
SeemaPatel-iViewLabs <seema.patel@iviewlabs.com>
2024-11-05 14:16:37 +0530

    account id update nd insert when user loggedin with soundcloud

--- packages/database/prisma/migrations/20241030095739_sound_cloud_account/migration.sql
+++ packages/database/prisma/migrations/20241030095739_sound_cloud_account/migration.sql
@@ -4,8 +4,10 @@ CREATE TABLE `sound_cloud_account` (
     `access_token` VARCHAR(191) NOT NULL,
     `refresh_token` VARCHAR(191) NOT NULL,
     `user_id` INTEGER NOT NULL,
+    `soundCloudAccountId` INTEGER NOT NULL,
+     `username` VARCHAR(191) NOT NULL,
     `token_created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `token_updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-
+     
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
