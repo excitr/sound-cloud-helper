cdbd1a0bc2269d81e95f0791142b7b59f504f265
SeemaPatel-iViewLabs <seema.patel@iviewlabs.com>
2024-11-18 18:37:40 +0530

    remove return null in start activity

--- apps/web/src/app/api/auth/start-activity/route.ts
+++ apps/web/src/app/api/auth/start-activity/route.ts
@@ -4,25 +4,39 @@ import { NextResponse } from 'next/server';
 import { type LogActivity, prisma, type Prisma } from '@repo/database';
 import { logger } from '@repo/logger';
 import { startOfDay, endOfDay } from 'date-fns';
-import type { OptionsSchema } from '@/app/(unauthenticated)/home/type';
+import type { OptionsSchema, StartActivityResponseData } from '@/app/(unauthenticated)/home/type';
 import { DAILY_MAX_FOLLOW_LIMIT } from '@/app/modules/constant';
 import { getAccountIdFromCookie, getSoudCloudeTokenFromCookie } from '@/app/lib/common-functions';
 import { fetchScrapUserData } from './action';
 
-export async function POST(request: Request): Promise<NextResponse | null> {
+export async function POST(request: Request): Promise<NextResponse<StartActivityResponseData>> {
   try {
     const body = (await request.json()) as OptionsSchema;
 
     const accessToken = await getSoudCloudeTokenFromCookie();
 
     if (!accessToken) {
-      return null;
+      return NextResponse.json({
+        success: false,
+        error: 'Not found access token',
+        scrapUrlData: null,
+        currentLogData: null,
+        lastLogData: null,
+        completedCountSum: 0,
+      });
     }
 
     const accountId = await getAccountIdFromCookie();
 
     if (!accountId) {
-      return null;
+      return NextResponse.json({
+        success: false,
+        error: 'Not found account token',
+        scrapUrlData: null,
+        currentLogData: null,
+        lastLogData: null,
+        completedCountSum: 0,
+      });
     }
 
     const today = new Date();
@@ -45,7 +59,14 @@ export async function POST(request: Request): Promise<NextResponse | null> {
     const lastLogData = todayLogActivities.length > 0 ? todayLogActivities[0] : null;
 
     if (completedCountSum >= DAILY_MAX_FOLLOW_LIMIT) {
-      return NextResponse.json({ scrapUrlData: null, currentLogData: null, lastLogData, completedCountSum });
+      return NextResponse.json({
+        success: true,
+        scrapUrlData: null,
+        currentLogData: null,
+        lastLogData: null,
+        completedCountSum,
+        error: null,
+      });
     }
 
     const data: Prisma.LogActivityCreateInput = {
@@ -61,9 +82,34 @@ export async function POST(request: Request): Promise<NextResponse | null> {
 
     const result = await fetchScrapUserData(accessToken, body.scrap_url);
 
-    return NextResponse.json({ scrapUrlData: result, currentLogData, lastLogData, completedCountSum });
+    const formattedCurrentLogData = {
+      ...currentLogData,
+      startTime: currentLogData.startTime.toISOString(), // Convert to ISO string
+      endTime: currentLogData.endTime.toISOString() || null, // Handle nullable endTime
+    };
+    return NextResponse.json({
+      success: true,
+      scrapUrlData: result,
+      currentLogData: formattedCurrentLogData,
+      lastLogData: lastLogData
+        ? {
+            ...lastLogData,
+            startTime: lastLogData.startTime.toISOString(),
+            endTime: lastLogData.endTime.toISOString() || null,
+          }
+        : null,
+      completedCountSum,
+      error: null,
+    });
   } catch (error) {
     logger.error(error, `Start Activity POST request error`);
-    return NextResponse.json(error);
+    return NextResponse.json({
+      success: false,
+      error: 'Error on start activity',
+      scrapUrlData: null,
+      currentLogData: null,
+      lastLogData: null,
+      completedCountSum: 0,
+    });
   }
 }
